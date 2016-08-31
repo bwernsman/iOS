7b1996e5def5be615ab425a5a356b8b4f8227531
Ben Wernsman <benwernsman@wireless-10-147-254-94.public.utexas.edu>
2016-08-31 13:53:02 -0500

    Updated Create
    -added static to create so it could be called from another class
    -talked to prof

--- HW1/WernsmanBenjamin-HW1/WernsmanBenjamin-HW1/Book.swift
+++ HW1/WernsmanBenjamin-HW1/WernsmanBenjamin-HW1/Book.swift
@@ -1,6 +1,8 @@
 //
 //  Book.swift
 //  WernsmanBenjamin-HW1
+//  EID:  bw22494
+//  Course:  CS378
 //
 //  Created by Ben Wernsman on 8/30/16.
 //  Copyright © 2016 wernsman. All rights reserved.
@@ -9,79 +11,81 @@
 import Foundation
 
 class Book{
-    private var title:String = ""
-    private var author:String = ""
-    private var numberOfPages:Int = 0
-    private var bookType:String = ""
-    private var price:Float = 0.0
+    
+    //Define variables (title, author, number of pages, type of book and the price)
+    private var title:String
+    private var author:String
+    private var numberOfPages:Int
+    private var bookType:String
+    private var price:Float
     
     //Initialize the variables
     init(title:String, author:String, numberOfPages:Int, bookType:String, price:Float){
         self.title = title
         self.author = author
         self.numberOfPages = numberOfPages
-        self.bookType = checkBookType(bookType)
+        self.bookType = bookType
         self.price = price
     }
     
-    //All the getters and setters
+    //Create a Book and return it
+    static func create(title:String, author:String, numberOfPages:Int, bookType:String, price:Float) -> Book{
+        return Book(title: title,author: author,numberOfPages: numberOfPages,bookType: bookType,price: price)
+    }
+    
+    //Set the title
     func setTitle(title:String) {
         self.title = title
     }
     
+    //Get the title
     func getTitle() -> String {
         return self.title
     }
     
+    //Set the author
     func setAuthor(author:String) {
         self.author = author
     }
     
+    //Get the author
     func getAuthor() -> String {
         return self.author
     }
     
+    //Set the number of pages
     func setNumberOfPages(numberOfPages:Int) {
         self.numberOfPages = numberOfPages
     }
     
+    //Get the number of pages
     func getNumberOfPages() -> Int {
         return self.numberOfPages
     }
     
+    //Set the book type
     func setBookType(bookType:String) {
         self.bookType = bookType
     }
     
+    //Get the book type
     func getBookType() -> String {
         return self.bookType
     }
     
+    //Set the price
     func setPrice(price:Float) {
         self.price = price
     }
     
+    //Get the price
     func getPrice() -> Float {
         return self.price
     }
     
-    //Check to make sure bookType is valid
-    func checkBookType(bookType:String) -> String {
-        if(bookType == "hardcover" || bookType == "softcover" || bookType == "eBook"){
-            return bookType
-        }
-        return "ERROR"
-    }
-    
-    //Return all the values as a String
+    //Return all the variables as a String
     func description() -> String{
-        return "Title: \"" + self.title + "\"\n# Pages: " + String(self.numberOfPages) + "\nBook Type: " + self.bookType + "\nPrice: " + String(self.price)
+        return "Title: \"" + self.title + "\"\nAuthor: " + self.author + "\n# Pages: " + String(self.numberOfPages) + "\nBook Type: " + self.bookType + "\nPrice: " + String(self.price)
      }
-    
-    //Create a book and return it
-    //ASK PROF ABOUT RETURNING
-    func create(title:String, author:String, numberOfPages:Int, bookType:String, price:Float) -> Book{
-        return Book(title: title,author: author,numberOfPages: numberOfPages,bookType: bookType,price: price)
-    }
     
 }
--- HW1/WernsmanBenjamin-HW1/WernsmanBenjamin-HW1/main.swift
+++ HW1/WernsmanBenjamin-HW1/WernsmanBenjamin-HW1/main.swift
@@ -1,6 +1,8 @@
 //
 //  main.swift
 //  WernsmanBenjamin-HW1
+//  EID:  bw22494
+//  Course:  CS378
 //
 //  Created by Ben Wernsman on 8/30/16.
 //  Copyright © 2016 wernsman. All rights reserved.
@@ -8,6 +10,7 @@
 
 import Foundation
 
+//Define variables for Books (books storage, title, author, number of pages, type of book and the price)
 var bookObjects:[Book] = []
 var titles:[String] = ["Moby Dick","Harry Potter and the Goblet of Fire","Justin Bieber: a Biography","Simple iOS Programming in Swift"]
 var authors:[String] = ["Herman Melville", "J.K. Rowling", "Selena Gomez", "Bill Bulko"]
@@ -15,19 +18,24 @@ var pageNumbers:[Int] = [1834,523,13,2145]
 var bookType:[String] = ["softcover","hardcover","eBook","hardcover"]
 var prices:[Float] = [10.00, 11.00, 12.00, 13.00]
 
-
+//Used to calculate the percentage increase in cost of a book
 //Changed var to let??!
 func randomUIntInRange(min:UInt32, max:UInt32) -> UInt32 {
     let randomValue:UInt32 = min + arc4random_uniform(UInt32(max - min + 1))
     return randomValue
 }
 
+//Creates 4 book objects
+//Changes the price 10X depending on the percentage calculated
+//Rounds the float to the nearest dollar value
+//Updates the current price
+//Prints out 4 books
 func main(){
     for index in 0...3 {
-        bookObjects.append(Book(title: titles[index], author: authors[index], numberOfPages: pageNumbers[index], bookType: bookType[index], price: prices[index]))
+        bookObjects.append(Book.create(titles[index], author: authors[index], numberOfPages: pageNumbers[index], bookType: bookType[index], price: prices[index]))
         for _ in 1 ... 10 {
-            let percentage:Float = Float(randomUIntInRange(0,max: 10))/100
             var price:Float = bookObjects[index].getPrice()
+            let percentage:Float = Float(randomUIntInRange(0,max: 10))/100
             price = (price * percentage) + price
             price = Float(round(100*price)/100)
             bookObjects[index].setPrice(price)
