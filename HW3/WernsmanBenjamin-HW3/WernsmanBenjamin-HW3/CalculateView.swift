1e7f14bfa75ea3f99ba7c923569adeab7d50f27e
Ben Wernsman <benwernsman@Bens-MacBook-Pro.local>
2016-09-14 13:04:25 -0500

    Added calculations
    -added calculations
    -added user input checks
    -added comments

--- HW3/WernsmanBenjamin-HW3/WernsmanBenjamin-HW3/CalculateView.swift
+++ HW3/WernsmanBenjamin-HW3/WernsmanBenjamin-HW3/CalculateView.swift
@@ -11,33 +11,81 @@ import UIKit
 
 class CalculateView: UIViewController {
     
+    // Use teamName to set label
+    var operatorName = String()
+    var operatorType = String()
+    
+    //Set Outlets
     @IBOutlet weak var opOne: UITextField!
     @IBOutlet weak var opTwo: UITextField!
     @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var op: UILabel!
     
+    //When the Calculate Button is pressed, make sure the inputs are valid
+    //If the inputs are valid, calculate the expected response and set it to the label "result"
+    //Dismiss keyboard after the result shows up
     @IBAction func calcButton(sender: AnyObject) {
-
+        let opOneInput:(Double,Bool) = checkInput(opOne.text!)
+        let opTwoInput:(Double,Bool) = checkInput(opTwo.text!)
         
-        print(opOne.text!)
+        if(opOneInput.1 && opTwoInput.1){
+            result.text! = String(calcInput(opOneInput.0,opTwoInput: opTwoInput.0))
+        }
+        
+        self.view.endEditing(true)
     }
     
-    // Use teamName to set label
-    var operatorType = String()
-    
+    //When the view loads, set the keybaord to "numbers"
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        opOne.keyboardType = UIKeyboardType.DecimalPad
+        opTwo.keyboardType = UIKeyboardType.DecimalPad
     }
     
+    //When the view appears, set the title and operator
     override func viewWillAppear(animated: Bool) {
-        self.title = operatorType
+        self.title = operatorName
+        op.text = operatorType
     }
     
+    //Make sure input is valid
+    func checkInput(input:String) -> (Double,Bool){
+        if let val = Double(input){
+            return (val,true)
+        }
+        else{
+            showError("Please enter a valid number")
+            return (0,false)
+        }
+    }
+    
+    //Calcualte the expected result
+    func calcInput(opOneInput:Double, opTwoInput:Double) -> Double{
+        if(operatorType == "+"){
+            return opOneInput + opTwoInput
+        }
+        else if (operatorType == "-"){
+            return opOneInput - opTwoInput
+        }
+        else if(operatorType == "*"){
+            return opOneInput * opTwoInput
+        }
+        else{
+            return opOneInput / opTwoInput
+        }
+    }
+    
+    //If the user gives an invalid input, alert them
+    func showError(errorMessage:String){
+        let alertController = UIAlertController(title: "ERROR", message: errorMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    //Check if there is a memory warning
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-    
 }
 
--- HW3/WernsmanBenjamin-HW3/WernsmanBenjamin-HW3/ViewController.swift
+++ HW3/WernsmanBenjamin-HW3/WernsmanBenjamin-HW3/ViewController.swift
@@ -8,18 +8,28 @@
 import Foundation
 import UIKit
 
-public let calcOperators = [
-    "Add", "Subtract", "Multiply", "Divide",
-]
-
-let textCellIdentifier = "TextCell"
-
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
+    //Connect the table view to the storyboard
     @IBOutlet weak var tableView: UITableView!
     
-    var operatorType:String = ""
+    //Set the text cell identifier
+    let textCellIdentifier = "TextCell"
     
+    //Create 2 arrays, one for the operator names and the other for the operator label in the next view
+    let calcOperatorNames = [
+        "Add", "Subtract", "Multiply", "Divide",
+    ]
+    
+    let calcOperators = [
+        "+", "-", "*", "/"
+    ]
+    
+    //Set values to pass to the next view
+    var operatorName:String = ""
+    var operatorType:String = ""
+
+    //When the view loads, set the title, the delegate and the datasource
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,46 +38,47 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    //Set the number of sections
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
+    //Set the number of rows
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return calcOperators.count
+        return calcOperatorNames.count
     }
     
+    //Set the info in the table view
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath)
         
         let row = indexPath.row
-        cell.textLabel?.text = calcOperators[row]
+        cell.textLabel?.text = calcOperatorNames[row]
         
         return cell
     }
     
-    
-    // No methods are required to conform to UITableViewDelegate,
-    // because all methods in the protocol are optional.
-    
+    //When a row is tapped, collect the info and store it so we can pass it to the next view
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        
+        operatorName = calcOperatorNames[indexPath.row]
         operatorType = calcOperators[indexPath.row]
         self.performSegueWithIdentifier("teamSegueIdentifier", sender: self)
     }
     
-    // MARK: - Navigation
+    //When transitioning to a new view, make sure that we send the operator name and operator type
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if(segue.identifier == "teamSegueIdentifier"){
             let destination = segue.destinationViewController as! CalculateView
+            destination.operatorName = operatorName
             destination.operatorType = operatorType
         }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 }
 
