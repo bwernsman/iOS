35fed3f807e0e91e0d807669e8c65420aefd2a90
Ben Wernsman <ben.wernsman@me.com>
2016-10-13 13:09:59 -0500

    HW7 Initial Commit
    -created project for HW7
    -added background color

---
+++ HW7/BenjaminWernsman-HW7/BenjaminWernsman-HW7/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BenjaminWernsman-HW7
+//
+//  Created by Ben Wernsman on 10/13/16.
+//  Copyright © 2016 utcs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor(red: 149/255, green: 237/255, blue: 252/255, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
